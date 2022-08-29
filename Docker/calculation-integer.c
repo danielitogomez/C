ee2b1bb4261fc9a4d048751c63eb37b4421e5304
danielitogomez <daniel.gomez.amaran@gmail.com>
2022-08-28 21:05:36 -0400

    feat: add new learning codes

---
+++ Docker/calculation-integer.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("Dear Procrastinator,\n");
+    printf("You still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!", 25-23, 60 * 24 * (25-23), 60 * 60 * 24 * (25-23));
+    return 0;
+}
---
+++ Docker/integer.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("3+2 equals %d and 3-2 equals %d and 3*2 equals %d\n", 3+2, 3-2, 3*2);
+    printf("3+2*3 equals %d and (3+2)*3 equals %d\n", 3+2*3, (3+2)*3);    
+    return 0;
+}
