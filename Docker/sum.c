8348e499b30a5ae6d65c2da2acf7d47be3345591
danielitogomez <daniel.gomez.amaran@gmail.com>
2022-12-28 19:00:59 +0100

    feat: add new function

---
+++ Docker/sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int pollution = 0;
+    pollution = 34;
+    printf("I have %d \n",pollution);
+    pollution = pollution + 34;
+    printf("I have %d \n",pollution);
+    pollution = pollution - 8;
+    pollution = pollution - 20;
+    printf("I have %d \n",pollution);
+    pollution = 10;
+    pollution = pollution + 12;
+    printf("I have %d \n",pollution);
+    return 0;
+}
